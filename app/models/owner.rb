28ec34f8c99c403384444ad7424c5f442268c366
jendobes <jdobak1@gmail.com>
2018-04-30 16:47:52 -0400

    added validations for email

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,6 +1,9 @@
 class Owner < ActiveRecord::Base
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  validates :email, :presence => true
+  validates :email, :uniqueness => true
+
 
   has_many :rescues
   has_many :pets, through: :rescues

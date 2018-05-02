686a589fb80076a28f621c56c7f433f90f326d85
jendobes <jdobak1@gmail.com>
2018-05-02 12:50:14 -0400

    added rescues as a nested resource for owners and pets

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :owners
-  resources :pets
+  resources :owners do
+    resources :rescues
+  end
+
+  resources :pets do
+    resources :rescues
+  end
 
   get '/signin', to: 'sessions#new', as: 'signin'
   post '/session', to: 'sessions#create', as: 'session'

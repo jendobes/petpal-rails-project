a5581888c216c7f3d5a16fb42a6481ecda478668
jendobes <jdobak1@gmail.com>
2018-05-30 11:18:45 -0400

    delete commented out blocks

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  # skip_before_action :verify_user_is_authenticated, only: [:new,:create]
+
   def new
     @user = Owner.new
   end
@@ -10,11 +10,6 @@ class SessionsController < ApplicationController
         u.name = auth['info']['name']
         u.email = auth['info']['email']
         u.password = SecureRandom.hex
-     # u.image = auth['info']['image']
-     # avatar_url = process_uri(auth.info.image)
-     # u.update_attribute(:image, URI.parse(avatar_url))
-     # u.image = URI.parse(avatar_url)
-     # u.image = URI.parse(auth.info.image)
       end
       session[:user_id] = @user.id
       redirect_to owner_path(@user)
@@ -41,13 +36,5 @@ class SessionsController < ApplicationController
   def auth
     request.env['omniauth.auth']
   end
-
-  # def process_uri(uri)
-  #   require 'open-uri'
-  #   require 'open_uri_redirections'
-  #   open(uri, :allow_redirections => :safe) do |r|
-  #     r.base_uri.to_s
-  #   end
-  # end
-
+  
 end

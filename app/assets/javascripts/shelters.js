7106665c9ff8f7925658fe519e0327dcdebb3499
jendobes <jdobak1@gmail.com>
2018-05-31 11:55:23 -0400

    populate shelters pet index dyamically with ajax

--- app/assets/javascripts/shelters.js
+++ app/assets/javascripts/shelters.js
@@ -2,10 +2,27 @@ $(document).ready(function() {
   attachListeners();
 });
 
+// function attachListeners() {
+//   $("#showPets").on('click', function(e) {
+//     $.get(this.href, function(data) {
+//       data.forEach(console.log(pet))
+//     }
+//     e.preventDefault()
+//   })
+// }
+
 function attachListeners() {
-  $("#petIndex").on('click', function(e) {
-    alert("holy shit this works ")
-    console.log("i was loaded")
+  $("#showPets").on('click', function(e) {
+    $.get(this.href, function(data) {
+      data.forEach(pet => {populateIndex(pet)})
+    })
+    $("#showPets").hide()
     e.preventDefault()
   })
+}
+
+
+function populatetIndex(pet) {
+  petLink = pet.name.link(`/pets/${pet.id}`)
+  $("#petIndex").append(petLink).append("<br>")
 }

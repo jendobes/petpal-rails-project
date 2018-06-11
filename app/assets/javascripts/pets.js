12c0d5b773317ef4ed35b8248a27008c1448c9e0
jendobes <jdobak1@gmail.com>
2018-06-11 12:28:42 -0400

    created event listener for submit rescue form on pet show page

--- app/assets/javascripts/pets.js
+++ app/assets/javascripts/pets.js
@@ -7,8 +7,20 @@ attachListeners = () => {
   $("#petRescue").on('click', function(e) {
     e.preventDefault()
     let element = $("#rescueForm")
-    element.show()
-    element.scrollIntoView();
+      element.show()
+      element.scrollIntoView();
+  })
+  attachSubmitListener()
+}
 
+attachSubmitListener = () => {
+  console.log("i was called")
+  $("#rescueSubmit").on('click', function(e) {
+    e.preventDefault()
+    alert("you clicked submit!")
   })
 }
+//hijack submit event of form
+//take the form data and send it as an ajax post request. url of post request?
+//create rescue from that post request
+//send back json of the rescue that was added and inject it in the DOM
